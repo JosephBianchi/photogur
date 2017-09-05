90556c4775cc9a8b05f2b10985b0342e832d4863
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-05 18:08:56 -0400

    done and doner

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,17 @@
 class Picture < ActiveRecord::Base
 
+  validates :artist, presence: true
+  validates :title, length: {minimum: 2}
+  validates :title, length: {maximum: 20}
+  validates :url, presence: true
+  validates :url, uniqueness: true
+
+
+
+
+
+
+
   def self.newest_first
      Picture.order("created_at DESC")
    end
