95988e9e570c740727281be50f085dadbd97330d
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-11 15:39:58 -0400

    sorry forgot to commit periodically - but done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password = params[:user][:password_confirmation]
+
+    if @user.save
+      redirect_to pictures_path
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
 
   delete 'pictures/:id' => 'pictures#destroy'
 
+  resources :users
+
+  resources :sessions
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
